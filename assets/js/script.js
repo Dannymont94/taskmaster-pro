66d97ecc8469ac511075c7819685569057db2769
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-02 00:15:54 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,6 +176,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function() {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function() {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function() {
+    $(this).addClass("dropover-active");
+  },
+  out: function() {
+    $(this).removeClass("dropover-active");
+  },
   update: function(event) {
     // array to store the task data in
     var tempArr = [];
@@ -223,7 +237,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -251,10 +265,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
